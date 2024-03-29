aa3c83611909736456db972ea6390e92167a0e5a
aya <aya@aya-mba.local>
2024-03-29 16:17:00 +0900

    middleware NotSubscribedを実装

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\Subscribed::class,
+        \App\Http\Middleware\NotSubscribed::class,
     ];
 
     /**
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'subscribed' => \App\Http\Middleware\Subscribed::class,
+        'notsubscribed' => \App\Http\Middleware\NotSubscribed::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::controller(UserController::class)->group(function () {
     Route::get('user/mypage', 'mypage')->name('mypage');
     Route::get('user/mypage/edit', 'edit')->name('mypage.edit');
     Route::put('user/mypage', 'update')->name('mypage.update');
-    Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
     Route::get('users/delete', 'delete')->name('user.delete');
     Route::delete('users/delete', 'destroy')->name('mypage.destroy');
 });
@@ -36,31 +35,24 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
     Route::get('/review', [ReviewController::class, 'index'])->name('restaurants.review');
 
+    //サブスクに移行する//
+    Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');   
+});
+
+Route::middleware(['notsubscribed'])->group(function () {
     Route::get('/subscription', [SubscriptionController::class, 'create'])->name('subscription.create');
     Route::post('/subscription/store', [SubscriptionController::class, 'store'])->name('subscription.store');
-
-
-    //サブスクに移行する//
-    Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
-    
 });
 
 Route::middleware(['subscribed'])->group(function () {
-
-   
+    Route::get('users/mypage/favorite', [UserController::class, 'favorite'])->name('mypage.favorite');
     Route::post('favorites/{restaurant_id}', [FavoriteController::class, 'store'])->name('favorites.store');
     Route::delete('favorites/{restaurant_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
-
-    
     Route::get('/subscription/edit', [SubscriptionController::class, 'edit'])->name('subscription.edit');
     Route::patch('/subscription/update', [SubscriptionController::class, 'update'])->name('subscription.update');
     Route::get('/subscription/cancel', [SubscriptionController::class, 'cancel'])->name('subscription.cancel');
     Route::post('/subscription/cancel', [SubscriptionController::class, 'destroy'])->name('subscription.destroy');
     
 });
-
-
-
-
 
 require __DIR__.'/auth.php';
